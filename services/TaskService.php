6a4f237faf647d84448d26ac10683e9e06b7f0c5
matheusfw <devbymatheus@gmail.com>
2026-05-05 15:12:29 -0300

    definindo conversão de array para json -> json_encode e criando métodos para carregar e salvar arquivos

--- services/TaskService.php
+++ services/TaskService.php
@@ -4,6 +4,10 @@ require_once __DIR__ . '/../models/Task.php';
 class TaskService {
     private $tasks = [];
 
+    public function __construct() {
+        $this->carregarEmArquivo();
+    }
+
     public function adicionar(Task $task) {
         $this->tasks[] = $task;
     }
@@ -27,6 +31,38 @@ class TaskService {
         }
 
         return $total;
+    }
+
+    public function salvarEmArquivo() {
+        $arquivo = __DIR__ . '/../data/tasks.json';
+
+        $dados = [];
+
+        foreach ($this->tasks as $task) {
+            $dados[] = [
+                'titulo' => $task->getTitulo(),
+                'concluida' => $task->getStatus()
+            ];
+        }
+
+        file_put_contents($arquivo, json_encode($dados, JSON_PRETTY_PRINT));
+    }
+
+    public function carregarEmArquivo() {
+        $arquivo = __DIR__ . '/../data/tasks.json';
+
+        if (!file_exists($arquivo)) {
+            return;
+        }
+
+        $json = file_get_contents($arquivo);
+        $dados = json_decode($json, true);
+
+        foreach ($dados as $item) {
+            $task = new Task($item['titulo']);
+            $task->setStatus(($item['concluida']));
+            $this->tasks[] = $task;
+        }
     }
 }
 ?>
